4c0eed1e132f16f3d3ac33a16f56a14299a54b72
Abhi088 <abhicoding8@gmail.com>
2021-06-19 22:25:58 +0530

    .

---
+++ routes/campgrounds.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const campgrounds = require('../controllers/campgrounds');
+const catchAsync = require('../utils/catchAsync');
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.single('image'), (req, res) => {
+        console.log(req.body, req.file);
+        res.send('joki');
+    })
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
+
+module.exports = router;
